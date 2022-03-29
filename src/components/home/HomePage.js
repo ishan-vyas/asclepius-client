893d11fed9133f7ebfb2ce3cc2fb71367de0fd1c
Ishan Vyas <ishan.official01@gmail.com>
2022-03-29 00:00:52 -0600

    home page changes

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,7 +3,12 @@ import React, { useState } from 'react';
 // import AppBar from '@material-ui/core/AppBar';
 // import Toolbar from '@material-ui/core/Toolbar';
 // import Typography from '@material-ui/core/Typography';
-import NavBar from '../UI/NavBar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { Link } from "react-router-dom";
 import './HomePage.css';
 
 const HomePage = (props) => {
@@ -12,8 +17,25 @@ const HomePage = (props) => {
 
     return (
         <div className='MainApp'>
-            <NavBar />
-            <h1>UNAUTHORIZED</h1>
+            <div className='nav-bar-container'>
+                <AppBar position="static">
+                    <Toolbar>
+                    <Typography className='app-title-nav' variant="h6" noWrap>
+                        <Link to='/home' className='home-btn-nav'>
+                            Asclepius
+                        </Link>
+                    </Typography>
+                    <div className='pages-nav-navbar'>
+                        <Link to='/login' className='logout-btn-nav'>
+                            <Button variant="outlined" color="primary">
+                                Log In
+                            </Button>
+                        </Link>
+                    </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            <h1>Asclepius</h1>
         </div>
         
     )
